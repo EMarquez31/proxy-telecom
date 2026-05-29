8e16dc84a7cca062b9dd324b57cdfdfe70f03355
Esteban Márquez Jaramillo <78503921+EMarquez31@users.noreply.github.com>
2026-05-29 17:03:39 -0500

    Update index.js

--- index.js
+++ index.js
@@ -21,9 +21,10 @@ app.get('/cliente', async (req, res) => {
       }
     )
     const texto = await response.text()
-    const contrato = texto.match(/^(\d+)/)?.[1] || null
-    const refPag = texto.match(/Ref\.Pag\.:(\S+)/)?.[1] || null
-    const dirSer = texto.match(/Dir\.Ser\.:\s*(.+)$/)?.[1]?.trim() || null
+    const texto2 = texto.replace(/["\[\]{}\\]/g, '').trim()
+    const contrato = texto2.match(/^(\d+)/)?.[1] || null
+    const refPag = texto2.match(/Ref\.Pag\.:(\S+)/)?.[1] || null
+    const dirSer = texto2.match(/Dir\.Ser\.:\s*(.+?)(\s*)$/)?.[1]?.trim() || null
     res.json({ contrato: contrato, referencia_pago: refPag, direccion_servicio: dirSer })
   } catch (e) {
     res.status(500).json({ error: 'Error consultando la API' })
